6e42538d4b4aa218d4521c2e5ab88bdc0766d31b
benscott <ben@benscott.co.uk>
2014-04-01 22:02:28 +0100

    DWC class

--- ckanext/nhm/lib/dwc.py
+++ ckanext/nhm/lib/dwc.py
@@ -8,13 +8,13 @@ Copyright (c) 2013 'bens3'. All rights reserved.
 import os
 from rdflib import Graph, RDF, RDFS, Namespace, URIRef
 
-DWC = Namespace('http://rs.tdwg.org/dwc/terms/')
-DWCA = Namespace('http://rs.tdwg.org/dwc/terms/attributes/')
-
 class DwC(object):
 
     g = Graph()
     g.load(os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.path.pardir)), 'src', 'dwcterms.rdf'))
+
+    DWC = Namespace('http://rs.tdwg.org/dwc/terms/')
+    DWCA = Namespace('http://rs.tdwg.org/dwc/terms/attributes/')
 
     def __init__(self, **kwargs):
 
@@ -38,12 +38,12 @@ class DwC(object):
         """
         try:
             # Find the subject in the graph
-            s, p, o = self.g.triples((URIRef(DWC.term(name)), RDF.type, RDF.Property)).next()
+            s, p, o = self.g.triples((URIRef(self.DWC.term(name)), RDF.type, RDF.Property)).next()
 
             # And return a dictionary of key terms
             return {
                 'uri': s,
-                'group': self.g.value(s, DWCA.organizedInClass),
+                'group': self.g.value(s, self.DWCA.organizedInClass),
                 'label': self.g.label(s).value
             }
 
@@ -53,7 +53,7 @@ class DwC(object):
     def get_group_terms(self, group):
 
         if not isinstance(group, URIRef):
-            group = URIRef(DWC.term(group))
+            group = URIRef(self.DWC.term(group))
 
         for name, term in self.__dict__.items():
 
