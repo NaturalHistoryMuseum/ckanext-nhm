b437a4bd4077abc583b1255ebffbfd1203a47f32
benscott <ben@benscott.co.uk>
2014-04-02 21:18:45 +0100

    xsd for dwc validation

--- ckanext/nhm/lib/dwc.py
+++ ckanext/nhm/lib/dwc.py
@@ -6,112 +6,96 @@ Copyright (c) 2013 'bens3'. All rights reserved.
 """
 
 import os
-from rdflib import Graph, RDF, Namespace, URIRef
-
-from rdflib.namespace import DCTERMS
+from lxml import etree
 from collections import OrderedDict
-
-# Darwin core namespaces
-DWC = Namespace('http://rs.tdwg.org/dwc/terms/')
-DWCA = Namespace('http://rs.tdwg.org/dwc/terms/attributes/')
+import re
 
 class DwC(object):
 
-    g = Graph()
-    g.load(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src', 'dwcterms.rdf'))
+    # All DwC terms
+    terms = OrderedDict()
 
-    # Create list of DwC properties on the same order as rdf graph
-    terms = []
-    for p in g.subjects(RDF.type, RDF.Property):
-        terms.append(p)
-
-    # TODO: Write test to ensure groups match
-    groups = OrderedDict([
-                (DWC.all, 'Specimen'),
-                (DWC.Taxon, 'Taxonomy'),
-                (DWC.Event, 'Event'),
-                (DCTERMS.Location, 'Location'),
-                (DWC.Identification, 'Identification'),
-                (DWC.Occurrence, 'Occurrence'),
-                (DWC.ResourceRelationship, 'Relationship'),
-                (DWC.GeologicalContext, 'Geology')
-            ])
-
-    __dict__ = {}
+    # Dictionary of group names, with a list of fields
+    groups = OrderedDict()
 
     def __init__(self, **kwargs):
 
-        # Set all the initial values
-        for name, value in kwargs.items():
-            self[name] = value
+        # Read the DwC terms XSD to populate terms and groups
+        f = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src', 'tdwg_dwcterms.xsd')
 
-    def __setitem__(self, name, value):
+        data = etree.parse(open(f), etree.XMLParser())
+        root = data.getroot()
 
-        # On setting a value, get the DwC term properties
-        if not name.startswith('_'):
-            term = self.get_term(name)
-            term['value'] = value
-            self.__dict__[name] = term
+        for group in root.iterfind("xs:group", namespaces=root.nsmap):
 
-    def __getitem__(self, name):
-        return self.__dict__[name]
+            group_label = self.get_group_label(group.get('name'))
+
+            # Create a list for the group terms
+            group_terms = []
+
+            for term in group.iterfind("xs:sequence/xs:element", namespaces=root.nsmap):
+
+                ns, name = term.get("ref").split(':')
+
+                try:
+                    value = kwargs.pop(name)
+                except KeyError:
+                    # If the value doesn't exist in kwargs, we don't care, it won't get added to __dict__
+                    pass
+                else:
+
+                    # Add to terms dictionary
+                    self.terms[name] = {
+                        'group': group_label,
+                        'uri': '{ns}{name}'.format(ns=root.nsmap[ns], name=name),
+                        'value': value
+                    }
+
+                    # Add to group terms
+                    group_terms.append(name)
+
+            # If we have terms for this group, add the group
+            # We don't want empty groups
+            if group_terms:
+                self.groups[group_label] = group_terms
+
+        # Do we have any kwargs left (excluding hidden fields)
+        unknown_terms = [k for k in kwargs if not k.startswith('_')]
+        if unknown_terms:
+            # If we do, they do not match any known DwC fields - so raise an error
+            raise ValueError('Unknown DwC term: %s' % ','.join(unknown_terms))
+
+    @staticmethod
+    def get_group_label(group_name):
+        """
+        Get a label for the group
+        Takes the original group name, removes Term, de-pluralises and splits on capital
+        GeologicalContextsTerm => Geological Contexts
+        @param group: Original group name
+        @return: label
+        """
+        label = ' '.join(re.findall('[A-Z][a-z]*', group_name.replace('Term', '')))
+
+        if label.endswith('s'):
+            label = label[:-1]
+
+        return label
+
+
 
     def get_groups(self):
         """
         @return: Generator of group names
         """
-        for group_uri, group in self.groups.items():
-            yield group_uri, group
+        for group, terms in self.groups.items():
+            yield group, terms
 
     def get_term(self, name):
         """
-        Get a DwC term definition
+        Get a term by name
+        @param name:
+        @return: term obj
         """
+        return self.terms[name]
 
-        # DC Terms are not included in RDF file, so handle separately
-        dc = ['created', 'modified']
 
-        if name in dc:
-            return {
-                'uri': URIRef(DCTERMS.term(name)),
-                'group': None,
-                'label': name.title(),
-                'index': 0 - dc.index(name)  # Create a -x index so doesn't clash with DwC
-            }
-
-        try:
-
-            # We want to keep track of the index, so we can order the fields as per DwC standard
-            i = self.terms.index(DWC.term(name))
-            s = self.terms[i]
-
-            # And return a dictionary of key terms
-            return {
-                'uri': s,
-                'group': self.g.value(s, DWCA.organizedInClass),
-                'label': self.g.label(s).value,
-                'index': i
-            }
-
-        except ValueError:
-            raise ValueError('Unknown DwC term: %s' % name)
-
-    def get_group_terms(self, group):
-
-        """ Return dictionary of terms in a group, using index for key
-        @param group:
-        @return: dict
-        """
-        terms = {}
-
-        if not isinstance(group, URIRef):
-            group = URIRef(DWC.term(group))
-
-        for term in self.__dict__.values():
-
-            if term['group'] == group:
-                terms[term['index']] = term
-
-        print sorted(terms)
-
-        return terms
