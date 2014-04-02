304a962da850f18e2c16c7880917feba4a552d8d
benscott <ben@benscott.co.uk>
2014-04-02 19:29:50 +0100

    Prior to etree

--- ckanext/nhm/lib/dwc.py
+++ ckanext/nhm/lib/dwc.py
@@ -6,17 +6,38 @@ Copyright (c) 2013 'bens3'. All rights reserved.
 """
 
 import os
-from rdflib import Graph, RDF, RDFS, Namespace, URIRef
+from rdflib import Graph, RDF, Namespace, URIRef
 
 from rdflib.namespace import DCTERMS
+from collections import OrderedDict
+
+# Darwin core namespaces
+DWC = Namespace('http://rs.tdwg.org/dwc/terms/')
+DWCA = Namespace('http://rs.tdwg.org/dwc/terms/attributes/')
 
 class DwC(object):
 
     g = Graph()
     g.load(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src', 'dwcterms.rdf'))
 
-    DWC = Namespace('http://rs.tdwg.org/dwc/terms/')
-    DWCA = Namespace('http://rs.tdwg.org/dwc/terms/attributes/')
+    # Create list of DwC properties on the same order as rdf graph
+    terms = []
+    for p in g.subjects(RDF.type, RDF.Property):
+        terms.append(p)
+
+    # TODO: Write test to ensure groups match
+    groups = OrderedDict([
+                (DWC.all, 'Specimen'),
+                (DWC.Taxon, 'Taxonomy'),
+                (DWC.Event, 'Event'),
+                (DCTERMS.Location, 'Location'),
+                (DWC.Identification, 'Identification'),
+                (DWC.Occurrence, 'Occurrence'),
+                (DWC.ResourceRelationship, 'Relationship'),
+                (DWC.GeologicalContext, 'Geology')
+            ])
+
+    __dict__ = {}
 
     def __init__(self, **kwargs):
 
@@ -33,45 +54,64 @@ class DwC(object):
             self.__dict__[name] = term
 
     def __getitem__(self, name):
-
         return self.__dict__[name]
+
+    def get_groups(self):
+        """
+        @return: Generator of group names
+        """
+        for group_uri, group in self.groups.items():
+            yield group_uri, group
 
     def get_term(self, name):
         """
         Get a DwC term definition
         """
-        if name in ['created', 'modified']:
+
+        # DC Terms are not included in RDF file, so handle separately
+        dc = ['created', 'modified']
+
+        if name in dc:
             return {
                 'uri': URIRef(DCTERMS.term(name)),
                 'group': None,
-                'label': name.title()
+                'label': name.title(),
+                'index': 0 - dc.index(name)  # Create a -x index so doesn't clash with DwC
             }
 
         try:
-            # Find the subject in the graph
-            s, p, o = self.g.triples((URIRef(self.DWC.term(name)), RDF.type, RDF.Property)).next()
+
+            # We want to keep track of the index, so we can order the fields as per DwC standard
+            i = self.terms.index(DWC.term(name))
+            s = self.terms[i]
 
             # And return a dictionary of key terms
             return {
                 'uri': s,
-                'group': self.g.value(s, self.DWCA.organizedInClass),
-                'label': self.g.label(s).value
+                'group': self.g.value(s, DWCA.organizedInClass),
+                'label': self.g.label(s).value,
+                'index': i
             }
 
-        except StopIteration:
-            raise AttributeError('Unknown DwC term: %s' % name)
+        except ValueError:
+            raise ValueError('Unknown DwC term: %s' % name)
 
     def get_group_terms(self, group):
 
-        """Generator returning terms in a particular group
+        """ Return dictionary of terms in a group, using index for key
         @param group:
         @return: dict
         """
+        terms = {}
 
         if not isinstance(group, URIRef):
-            group = URIRef(self.DWC.term(group))
+            group = URIRef(DWC.term(group))
 
-        for name, term in self.__dict__.items():
+        for term in self.__dict__.values():
 
             if term['group'] == group:
-                yield name, term
+                terms[term['index']] = term
+
+        print sorted(terms)
+
+        return terms
