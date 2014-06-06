5004b0af4f37f082f5c7e7a92c3d085db906116f
benscott <ben@benscott.co.uk>
2014-06-06 18:51:56 +0100

    dwc inherits from record

--- ckanext/nhm/controllers/dwc.py
+++ ckanext/nhm/controllers/dwc.py
@@ -6,6 +6,7 @@ import ckan.plugins as p
 from ckan.common import _, c
 import logging
 from ckanext.nhm.lib.dwc import DwC
+from ckanext.nhm.controllers.record import RecordController
 
 log = logging.getLogger(__name__)
 
@@ -18,7 +19,7 @@ NotAuthorized = logic.NotAuthorized
 ValidationError = logic.ValidationError
 get_action = logic.get_action
 
-class DarwinCoreController(base.BaseController):
+class DarwinCoreController(RecordController):
     """
     Controller for displaying KE EMu records as DwC
     """
@@ -31,34 +32,12 @@ class DarwinCoreController(base.BaseController):
         :param record_id:
         :return: html
         """
-        context = {'model': model, 'session': model.Session, 'user': c.user or c.author}
 
-        # Try & get the resource
-        try:
-            c.resource = get_action('resource_show')(context, {'id': resource_id})
+        self._load_data(package_name, resource_id, record_id)
 
-            if c.resource['format'] != 'dwc':
-                abort(404, _('No Darwin Core record found'))
+        c.dwc = DwC(**c.record_dict)
 
-            c.package = get_action('package_show')(context, {'id': package_name})
-            # required for nav menu
-            c.pkg = context['package']
-            c.pkg_dict = c.package
-            c.record_dict = get_action('record_get')(context, {'resource_id': resource_id, 'record_id': record_id, 'dwc': True})
-        except NotFound:
-            abort(404, _('Resource not found'))
-        except NotAuthorized:
-            abort(401, _('Unauthorized to read resource %s') % package_name)
+        if c.resource['format'] != 'dwc':
+            abort(404, _('Record not in Darwin Core format'))
 
-<<<<<<< HEAD
-        return p.toolkit.render('dwc/view.html', extra_vars={
-            'DwC': DwC,
-        })
-
-
-
-
-
-=======
         return p.toolkit.render('dwc/view.html')
->>>>>>> ckan-1251-1725-custom
