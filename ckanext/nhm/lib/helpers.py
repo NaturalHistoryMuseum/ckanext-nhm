2ad2907e50c9292344d3ac310757964e26707387
benscott <ben@benscott.co.uk>
2014-06-07 11:19:44 +0100

    Helper tidied up after moving to controller

--- ckanext/nhm/lib/helpers.py
+++ ckanext/nhm/lib/helpers.py
@@ -3,20 +3,8 @@ import logging
 import json
 import ckan.model as model
 import ckan.logic as logic
-from datetime import datetime
-import ckan.lib.formatters as formatters
-import urllib
-from collections import OrderedDict
-# All funcs will be made available as helpers
-from webhelpers.number import format_data_size
-import os
 
 log = logging.getLogger(__name__)
-
-MULTIMEDIA_URL = 'http://www.nhm.ac.uk/emu-classes/class.EMuMedia.php?irn=%s&image=yes'
-
-INSTITUTION_CODE = 'NHMUK'
-IDENTIFIER_PREFIX = '%s:ecatalogue:' % INSTITUTION_CODE
 
 def get_site_statistics():
     #TEMP: Just to put some stats on the home page
@@ -31,32 +19,6 @@ def get_site_statistics():
 
     return stats
 
-
-def record_display_name(record):
-    # Get the name to display for a record
-    # TODO: Need to have a form for adding what field to use
-    return record.get('title', None) or record.get('name', None) or record.get('name', None) or 'Record %s' % record.get('_id', 'Unknown')
-
-
-def fields_have_content(record, fields):
-    """
-    See if any one of a list fo fields have content
-    @param record:
-    @param fields: fields to check exist
-    @return: True
-    """
-    for field in fields:
-
-        if isinstance(field, tuple):
-            field = field[0]
-
-        if record.get(field, None):
-            return True
-
-
-def unique_identifier(id):
-    return '%s%s' % (IDENTIFIER_PREFIX, id)
-
 def get_record_point(rec):
 
     # TODO: What should happen if lat/lon not sane?
@@ -69,40 +31,20 @@ def get_record_point(rec):
 
     return None
 
-def parse_dynamic_properties(rec):
-
-    properties = {}
-
-    for prop in rec['dynamicProperties'].strip().split(';'):
-        try:
-            key, value = prop.strip().split('=')
-        except ValueError:
-            pass
-        else:
-
-            properties[key] = value
-
-    return properties
-
-
 def resource_view_state(resource_view_json):
     """
     Alter the recline view resource, adding in state info
     @param resource_view_json:
     @return:
     """
-
     resource_view = json.loads(resource_view_json)
     resource_view['state'] = {
         'fitColumns': True,
-        'enableCellRangeSelection': False,
-        'enableTextSelectionOnCells': False,
         'gridOptions': {
             'defaultFormatter': 'NHMFormatter',
             'enableCellRangeSelection': False,
             'enableTextSelectionOnCells': False,
             'enableCellNavigation': False,
-            'selectedCellCssClass': 'bugger'
         }
     }
 
