93b82dc09128d6423cad633d76dd9d085cc109b4
benscott <ben@benscott.co.uk>
2014-06-07 17:15:19 +0100

    Added helper to retrieve field names for a datastore resource

--- ckanext/nhm/lib/helpers.py
+++ ckanext/nhm/lib/helpers.py
@@ -3,8 +3,11 @@ import logging
 import json
 import ckan.model as model
 import ckan.logic as logic
+import ckan.plugins.toolkit as toolkit
 
 log = logging.getLogger(__name__)
+
+NotFound = logic.NotFound
 
 def get_site_statistics():
     #TEMP: Just to put some stats on the home page
@@ -19,6 +22,7 @@ def get_site_statistics():
 
     return stats
 
+
 def get_record_point(rec):
 
     # TODO: What should happen if lat/lon not sane?
@@ -30,6 +34,7 @@ def get_record_point(rec):
         })
 
     return None
+
 
 def resource_view_state(resource_view_json):
     """
@@ -49,3 +54,24 @@ def resource_view_state(resource_view_json):
     }
 
     return json.dumps(resource_view)
+
+
+def get_datastore_fields(resource_id):
+    """
+    Get list of fields for a resource
+    @param resource_id:
+    @return: list
+    """
+
+    data = {'resource_id': resource_id, 'limit': 0}
+    try:
+        result = toolkit.get_action('datastore_search')({}, data)['fields']
+        print result
+    except NotFound:
+        fields = []
+    else:
+        fields = [(f['id'], f['id'].title()) for f in result]
+
+    print fields
+
+    return fields
