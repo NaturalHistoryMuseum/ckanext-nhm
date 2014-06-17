2bf384ba554cc943dc3b4f1303bf7a9f4c738b60
benscott <ben@benscott.co.uk>
2014-06-17 13:23:54 +0100

    Added schema validators

---
+++ ckanext/nhm/logic/validators.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by 'bens3' on 2013-06-21.
+Copyright (c) 2013 'bens3'. All rights reserved.
+"""
+
+import ckan.plugins as p
+
+Invalid = p.toolkit.Invalid
+
+def string_max_length(max_length):
+    '''
+    Checks if a string is longer than a certain length
+
+    :raises: ckan.lib.navl.dictization_functions.Invalid if the string is
+        longer than max length
+    '''
+    def callable(value, context):
+
+        if len(value) > max_length:
+            raise Invalid(
+                _('Length must be less than {0} characters')
+                .format(max_length)
+            )
+
+        return value
+
+    return callable
