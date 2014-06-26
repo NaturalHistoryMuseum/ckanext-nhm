6bde7628b9c115ca9ffe80bc28841f32e8fb8748
benscott <ben@benscott.co.uk>
2014-06-26 12:08:45 +0100

    initdb command

---
+++ ckanext/nhm/commands/initdb.py
@@ -0,0 +1,23 @@
+
+import logging
+from ckan.plugins import toolkit
+import ckan.model as model
+from ckan.lib.cli import CkanCommand
+from ckanext.nhmstats.model import Base
+
+log = logging.getLogger()
+
+class InitDBCommand(CkanCommand):
+    """
+    Initialise the local stats database tables
+    """
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+    max_args = 0
+    min_args = 0
+
+    def command(self):
+
+        # Create the tables
+        self._load_config()
+        Base.metadata.create_all(model.meta.engine)
