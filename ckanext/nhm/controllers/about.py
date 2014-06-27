1f42d123de185024929f605e8d83f99edb2e9c42
benscott <ben@benscott.co.uk>
2014-06-27 10:58:41 +0100

    Tidied code to remove stats page

--- ckanext/nhm/controllers/about.py
+++ ckanext/nhm/controllers/about.py
@@ -14,31 +14,3 @@ class AboutController(p.toolkit.BaseController):
 
     def credits(self):
         return p.toolkit.render('about/credits.html', {'title': 'Credits'})
-
-    def about_stats(self):
-
-        # # ALl of these are taken from the stats module
-        # # We're re-writing it as we don't want all their content, and they haven't made it easy to override
-        # stats = stats_lib.Stats()
-        #
-        # rev_stats = stats_lib.RevisionStats()
-        #
-        # c.num_packages_by_week = rev_stats.get_num_packages_by_week()
-        # c.package_revisions_by_week = rev_stats.get_by_week('package_revisions')
-        # c.new_packages_by_week = rev_stats.get_by_week('new_packages')
-        #
-        # c.raw_packages_by_week = []
-        # for week_date, num_packages, cumulative_num_packages in c.num_packages_by_week:
-        #     c.raw_packages_by_week.append({'date': h.date_str_to_datetime(week_date), 'total_packages': cumulative_num_packages})
-        #
-        # c.raw_all_package_revisions = []
-        # for week_date, revs, num_revisions, cumulative_num_revisions in c.package_revisions_by_week:
-        #     c.raw_all_package_revisions.append({'date': h.date_str_to_datetime(week_date), 'total_revisions': num_revisions})
-        #
-        # c.raw_new_datasets = []
-        # for week_date, pkgs, num_packages, cumulative_num_packages in c.new_packages_by_week:
-        #     c.raw_new_datasets.append({'date': h.date_str_to_datetime(week_date), 'new_packages': num_packages})
-
-        return p.toolkit.render('about/stats.html', {'title': 'Statistics'})
-
-
