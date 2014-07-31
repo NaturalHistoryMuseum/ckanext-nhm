29a232678407e907753e967d77dccdf211e023ef
benscott <ben@benscott.co.uk>
2014-07-31 18:03:52 +0100

    Fixed slickgrid link bug

--- ckanext/nhm/theme/public/scripts/slickgrid.js
+++ ckanext/nhm/theme/public/scripts/slickgrid.js
@@ -3,6 +3,12 @@
 var pattern = /view\/[0-9a-z\-]+/;
 var baseURL = document.URL.replace(pattern, '')
 
+// Remove query string from BaseURL
+if (baseURL.indexOf('?') > -1){
+    baseURLparts = baseURL.split('?');
+    baseURL = baseURLparts[0]
+}
+
 // Slickgrid formatter for making the _id field into a link
 var NHMFormatter = function(row, cell, value, columnDef, dataContext) {
     if(columnDef.id == "del"){
@@ -10,7 +16,10 @@ var NHMFormatter = function(row, cell, value, columnDef, dataContext) {
     }
 
     if(columnDef.id == "_id"){
+
+        // Build URL, ensuring query is at the end
         var url = baseURL + 'record/' + value
+
         // Slickgrid uses preventDefault() preventing the link from working
         // So am using the onclick handler to change location
         return '<a target="_parent" href="' + url + '" onclick="window.top.location=this.href">View record</a>';
@@ -19,3 +28,7 @@ var NHMFormatter = function(row, cell, value, columnDef, dataContext) {
     return value
 
 };
+
+
+
+
