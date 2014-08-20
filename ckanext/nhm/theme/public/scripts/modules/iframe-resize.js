f555d3422ada1c77e7c36d88b6b96d962b8b5f76
Alice Heaton <a.heaton@nhm.ac.uk>
2014-08-20 15:15:57 +0100

    Improve iframe height change tracking

--- ckanext/nhm/theme/public/scripts/modules/iframe-resize.js
+++ ckanext/nhm/theme/public/scripts/modules/iframe-resize.js
@@ -3,10 +3,22 @@ this.ckan.module('iframe-resize', function(jQuery, _) {
   return {
     initialize: function() {
       var $iframe = this.el;
-      var i_window = $iframe.get(0).contentWindow;
-      var i_doc = i_window.document;
-      jQuery(i_window).resize(function(){
-        $iframe.height(jQuery(i_doc).height);
+      $iframe.ready(function(){
+        var i_window = $iframe.get(0).contentWindow;
+        var set_height = function(){
+          // body is not always available on some browsers. True even if we did a jQuery(i_window.document).ready(...)
+          if (i_window.document && i_window.document.body){
+            var current = $iframe.height();
+            var target = jQuery(i_window.document).height();
+            if (current != target){
+              $iframe.height(target);
+            }
+          }
+        };
+        set_height();
+        // This is, unfortunately, the only reliable cross browser way to track iframe size changes.
+        jQuery(i_window).resize(set_height);
+        window.setInterval(set_height, 250);
       });
     }
   }
