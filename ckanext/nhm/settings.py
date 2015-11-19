77ab8a74845a57329b63a9155d8fb2b56c5d9969
benscott <ben@benscott.co.uk>
2015-11-19 14:50:41 +0000

    Live emails

--- ckanext/nhm/settings.py
+++ ckanext/nhm/settings.py
@@ -4,21 +4,20 @@
 Few module settings which don't really need to go in ini file
 """
 
-
 # Contact addresses to route specimen contact request to
-COLLECTION_CONTACTS = {
-    'Entomology': 'b.scott@nhm.ac.uk',
-    'Botany': 'b.scott@nhm.ac.uk',
-    'Mineralogy': 'b.scott@nhm.ac.uk',
-    'Palaeontology': 'b.scott@nhm.ac.uk',
-    'Zoology': 'b.scott@nhm.ac.uk'
-}
+# COLLECTION_CONTACTS = {
+#     'Entomology': 'b.scott@nhm.ac.uk',
+#     'Botany': 'b.scott@nhm.ac.uk',
+#     'Mineralogy': 'b.scott@nhm.ac.uk',
+#     'Palaeontology': 'b.scott@nhm.ac.uk',
+#     'Zoology': 'b.scott@nhm.ac.uk'
+# }
 
 # For live
-# COLLECTION_CONTACTS = {
-#     'Entomology': 'adrian.hine@nhm.ac.uk',
-#     'Botany': 'lawrence.brooks@nhm.ac.uk',
-#     'Mineralogy': 'd.a.smith@nhm.ac.uk',
-#     'Palaeontology': 'd.a.smith@nhm.ac.uk',
-#     'Zoology': 't.conyers@nhm.ac.uk'
-# }
+COLLECTION_CONTACTS = {
+    'Entomology': 'adrian.hine@nhm.ac.uk',
+    'Botany': 'lawrence.brooks@nhm.ac.uk',
+    'Mineralogy': 'd.a.smith@nhm.ac.uk',
+    'Palaeontology': 'd.a.smith@nhm.ac.uk',
+    'Zoology': 't.conyers@nhm.ac.uk'
+}
