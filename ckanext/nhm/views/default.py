d694e66c407d0ab4d8306e042c353114b5273e45
benscott <ben@benscott.co.uk>
2017-09-27 16:04:43 +0100

    Fix for missing custom record fields

--- ckanext/nhm/views/default.py
+++ ckanext/nhm/views/default.py
@@ -5,9 +5,12 @@ Created by 'bens3' on 2013-06-21.
 Copyright (c) 2013 'bens3'. All rights reserved.
 """
 
-from ckanext.nhm.lib.resource import resource_get_ordered_fields
 import ckan.plugins as p
 from collections import OrderedDict
+import ckan.logic as logic
+
+
+NotFound = logic.NotFound
 
 
 class DefaultView(object):
@@ -55,6 +58,21 @@ class DefaultView(object):
         'columnsToolTip': []
     }
 
+    @staticmethod
+    def get_ordered_fields(resource_id):
+        """
+        Get fields ordered the same as the uploaded dataset
+        @param resource_id:
+        @return:
+        """
+        data = {'resource_id': resource_id, 'limit': 0}
+        try:
+            result = p.toolkit.get_action('datastore_search')({}, data)
+        except NotFound:
+            return []
+        else:
+            return [f['id'] for f in result['fields']]
+
     def render_record(self, c):
         """
         Render a record
@@ -63,7 +81,8 @@ class DefaultView(object):
         # The record_dict does not have fields in the correct order
         # So load the fields, and create an OrderedDict with field: value
         c.field_data = OrderedDict()
-        for field in resource_get_ordered_fields(c.resource['id']):
+
+        for field in self.get_ordered_fields(c.resource['id']):
             if not field.startswith('_'):
                 c.field_data[field] = c.record_dict.get(field, None)
 
