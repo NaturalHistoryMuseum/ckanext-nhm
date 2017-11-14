923ab87f45acfb6839b5dab43f13ad610f1c2a9e
benscott <ben@benscott.co.uk>
2017-11-14 15:31:13 +0000

    Clear cache on dataset updated

---
+++ ckanext/nhm/lib/cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by Ben Scott on '13/11/2017'.
+"""
+
+import logging
+import requests
+from pylons import config
+
+log = logging.getLogger(__name__)
+
+
+def cache_clear_nginx_proxy():
+    """
+    Clear NGINX Procy Cache - issue PURGE request to load balancer
+    @return:
+    @rtype:
+    """
+    url = config.get("ckan.site_url")
+
+    # Prepare a PURGE request to send to front end proxy
+    req = requests.Request('PURGE', url)
+    s = requests.Session()
+    r = s.send(req.prepare())
+    try:
+        r.raise_for_status()
+    except:
+        log.critical('Error clearing NGINX Cache')
