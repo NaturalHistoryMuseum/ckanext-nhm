e4eec66df9c7346c992b46b39a49f7cd617e2676
Alice Butcher <alycejenni@gmail.com>
2018-01-17 18:34:56 +0000

    tidyied setup.py
    changed author to Natural History Museum

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages, setup
 
-version = '0.3'
+version = '0.4'
 
 setup(
-	name='ckanext-nhm',
-	version=version,
-	description="",
-	long_description="""""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Ben Scott',
-	author_email='',
-	url='',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.nhm'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
-        [ckan.plugins]
-            nhm = ckanext.nhm.plugin:NHMPlugin
-
-	    [paste.paster_command]
-            dataset-category=ckanext.nhm.commands.dataset_category:DatasetCategoryCommand
-            initdb=ckanext.nhm.commands.initdb:InitDBCommand
-            datastore=ckanext.nhm.commands.datastore:DatastoreCommand
-            file=ckanext.nhm.commands.file:FileCommand
-
-        [ckan.rdf.profiles]
-            nhm_dcat=ckanext.nhm.dcat.profiles:NHMDCATProfile
-	""",
-)
+    name='ckanext-nhm',
+    version=version,
+    description="",
+    long_description="""""",
+    classifiers=[],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Natural History Museum',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.nhm'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+        ],
+    entry_points= \
+        """
+            [ckan.plugins]
+                nhm = ckanext.nhm.plugin:NHMPlugin
+    
+            [paste.paster_command]
+                dataset-category=ckanext.nhm.commands.dataset_category:DatasetCategoryCommand
+                initdb=ckanext.nhm.commands.initdb:InitDBCommand
+                datastore=ckanext.nhm.commands.datastore:DatastoreCommand
+                file=ckanext.nhm.commands.file:FileCommand
+    
+            [ckan.rdf.profiles]
+                nhm_dcat=ckanext.nhm.dcat.profiles:NHMDCATProfile
+        """,
+    )
