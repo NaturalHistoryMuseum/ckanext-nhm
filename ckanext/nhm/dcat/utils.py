47d95b6b3eda0b8dfce07f00af35e308defc9629
Josh Humphries <j.humphries@nhm.ac.uk>
2019-05-29 09:35:09 +0100

    Rewrite record rdf generation
    This is all now neater, easier to understand and conforms better with rdf outputs
    of our colleagues in other institutions. This commit also brings us in line with
    the CETAF CSPP recommendations: https://cetafidentifiers.biowikifarm.net/wiki/CSPP.

--- ckanext/nhm/dcat/utils.py
+++ ckanext/nhm/dcat/utils.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-Created by 'bens3' on 2013-06-21.
-Copyright (c) 2013 'bens3'. All rights reserved.
-"""
+from datetime import datetime
+from rdflib import namespace
 
-from ckanext.dcat.utils import catalog_uri
+from ckan.lib.helpers import url_for
 from ckanext.nhm.lib.helpers import get_specimen_resource_id
 
-def object_uri(record_dict):
+
+def object_uri(uuid, version=None):
     """
     Returns an URI for an object
-
-    This will be used to uniquely reference the dataset on the RDF
-    serializations.
-
-    Returns a string with the dataset URI.
     """
-
-    uuid = record_dict.get('uuid')
-    uri = '{0}/object/{1}'.format(catalog_uri().rstrip('/'), uuid)
-    return uri
+    if version is None:
+        return url_for(u'object_view', action=u'view', uuid=uuid, qualified=True)
+    else:
+        return url_for(u'object_view_versioned', action=u'view', uuid=uuid, version=version,
+                       qualified=True)
 
 
 def rdf_resources():
@@ -30,3 +25,81 @@ def rdf_resources():
     """
     # FIXME - Need to add in indexlots and artefacts
     return [get_specimen_resource_id()]
+
+
+def as_dwc_list(objects):
+    '''
+    Returns the given objects as a list using the DwC standard | style separator.
+
+    :param objects: the objects
+    :return: a | separated string
+    '''
+    return u' | '.join(objects)
+
+
+def epoch_to_datetime(epoch_timestamp):
+    '''
+    Converts the given epoch timestamp into a datetime object. The timestamp passed in is assumed to
+    be the integer number of milliseconds since the UNIX epoch.
+
+    :param epoch_timestamp: the integer number of milliseconds since the UNIX epoch
+    :return: a datetime object
+    '''
+    return datetime.fromtimestamp(epoch_timestamp / 1000.0)
+
+
+class Namespaces:
+    '''
+    Class representing the namespaces available to an RDF graph.
+    '''
+
+    def __init__(self, graph):
+        '''
+        :param graph: the graph object to bind the used namespaces to
+        '''
+        self.graph = graph
+
+        self.bound = set()
+        self.known_namespaces = {
+            u'adms': namespace.Namespace(u'http://www.w3.org/ns/adms#'),
+            u'aiiso': namespace.Namespace(u'http://purl.org/vocab/aiiso/schema#'),
+            u'cc': namespace.Namespace(u'http://creativecommons.org/ns#'),
+            u'dc': namespace.DCTERMS,
+            u'dcat': namespace.Namespace(u'http://www.w3.org/ns/dcat#'),
+            u'dqv': namespace.Namespace(u'http://www.w3.org/ns/dqv#'),
+            u'dwc': namespace.Namespace(u'http://rs.tdwg.org/dwc/terms/'),
+            u'foaf': namespace.FOAF,
+            u'locn': namespace.Namespace(u'http://www.w3.org/ns/locn#'),
+            u'gsp': namespace.Namespace(u'http://www.opengis.net/ont/geosparql#'),
+            u'mads': namespace.Namespace(u'http://www.loc.gov/mads/rdf/v1#'),
+            u'org': namespace.Namespace(u'http://www.w3.org/TR/vocab-org/#'),
+            u'owl': namespace.OWL,
+            u'rdf': namespace.RDF,
+            u'rdfs': namespace.RDFS,
+            u'schema': namespace.Namespace(u'http://schema.org/'),
+            u'sdmx_code': namespace.Namespace(u'http://purl.org/linked-data/sdmx/2009/code#'),
+            u'sdwc': namespace.Namespace(u'http://rs.tdwg.org/dwc/xsd/simpledarwincore#'),
+            u'skos': namespace.SKOS,
+            u'tdwgi': namespace.Namespace(u'http://rs.tdwg.org/ontology/voc/Institution#'),
+            u'time': namespace.Namespace(u'http://www.w3.org/2006/time'),
+            u'vcard': namespace.Namespace(u'http://www.w3.org/2006/vcard/ns#'),
+            u'void': namespace.VOID,
+        }
+
+    def __getattr__(self, prefix):
+        '''
+        Returns the namespace associated with the given prefix and ensures it is bound to the graph
+        if it hasn't been already.
+
+        :param prefix: the namespace prefix as defined in the known_namespaces dict attribute
+        :return: the namespace object
+        '''
+        if prefix in self.known_namespaces:
+            ns = self.known_namespaces[prefix]
+            if (prefix, ns) not in self.bound:
+                self.graph.bind(prefix, ns)
+                self.bound.add((prefix, ns))
+            return ns
+        else:
+            # if you get this error, just add it above!
+            raise ValueError(u'No known namespace with prefix {}'.format(prefix))
