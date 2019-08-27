536ac5dcd33e1dc20f90c16e8c7aaf33647f84f9
Alice Butcher <alycejenni@gmail.com>
2019-08-27 15:37:57 +0100

    A slightly hacky way to add git dependencies
    So either pip install or python setup.py can be used

--- setup.py
+++ setup.py
@@ -11,6 +11,30 @@ __version__ = u'1.0.0-alpha'
 with open(u'README.md', u'r') as f:
     __long_description__ = f.read()
 
+nhmgit = 'git+https://github.com/NaturalHistoryMuseum/'
+
+dependencies = {'ckanext-spatial': 'git+https://github.com/ckan/ckanext-spatial.git#egg=ckanext-spatial',
+'ckanext-viewhelpers': 'git+https://github.com/ckan/ckanext-viewhelpers.git#egg=ckanext_viewhelpers',
+'ckanext-dcat': 'git+https://github.com/ckan/ckanext-dcat.git#egg=ckanext_dcat',
+'ckanext-pdfview': 'git+https://github.com/ckan/ckanext-pdfview.git#egg=ckanext_pdfview',
+'ckanext-contact': nhmgit + 'ckanext-contact.git#egg=ckanext-contact',
+'ckanext-doi': nhmgit + 'ckanext-doi.git#egg=ckanext-doi',
+'ckanext-gallery': nhmgit + 'ckanext-gallery.git#egg=ckanext-gallery',
+'ckanext-userdatasets': nhmgit + 'ckanext-userdatasets.git#egg=ckanext-userdatasets',
+'ckanext-ldap': nhmgit + 'ckanext-ldap.git#egg=ckanext-ldap',
+'ckanext-graph': nhmgit + 'ckanext-graph.git#egg=ckanext-graph',
+'ckanext-ckanpackager': nhmgit + 'ckanext-ckanpackager.git#egg=ckanext-ckanpackager',
+'ckanext-video': nhmgit + 'ckanext-video.git#egg=ckanext_video',
+'ckanext-gbif': nhmgit + 'ckanext-gbif.git#egg=ckanext_gbif',
+'ckanext-statistics': nhmgit + 'ckanext-statistics.git#egg=ckanext_statistics',
+'ckanext-list': nhmgit + 'ckanext-list.git#egg=ckanext_list',
+'ckanext-status': nhmgit + 'ckanext-status.git#egg=ckanext_status',
+'ckanext-twitter': nhmgit + 'ckanext-twitter.git#egg=ckanext_twitter',
+'ckanext-sketchfab': nhmgit + 'ckanext-sketchfab.git#egg=ckanext_sketchfab',
+'ckanext-versioned-datastore': nhmgit + 'ckanext-versioned-datastore.git#egg=ckanext_versioned_datastore',
+'ckanext-versioned-tiledmap': nhmgit + 'ckanext-versioned-tiledmap.git#egg=ckanext_versioned_tiledmap',
+'ckanext-query-dois': nhmgit + 'ckanext-query-dois.git#egg=ckanext_query_dois',}
+
 setup(
     name=u'ckanext-nhm',
     version=__version__,
@@ -42,7 +66,8 @@ setup(
         'Shapely<1.3',
         'raven==6.0.0',
         'elasticsearch-dsl==6.2.1',
-        ],
+        ] + [u'{0} @ {1}'.format(k, v) for k, v in dependencies.items()],
+    dependency_links=dependencies.values(),
     entry_points= \
         u'''
         [ckan.plugins]
