bf4383330a340cd2ed6d2129de8fbf4a4eeb1c71
Alice Butcher <alycejenni@gmail.com>
2019-11-28 21:30:52 +0000

    Added directive for dismissing popups by clicking outside

--- ckanext/nhm/theme/fanstatic/scripts/apps/search/src/app.js
+++ ckanext/nhm/theme/fanstatic/scripts/apps/search/src/app.js
@@ -2,11 +2,39 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store/main';
 
+let outsideClick;
+
+Vue.directive('dismiss', {
+    bind(el, binding, vnode) {
+        outsideClick = (event) => {
+            console.log(event.target);
+            event.stopPropagation();
+            let ignore = binding.value.ignore.includes(event.target.id);
+            let i = 0;
+            while (!ignore && i < binding.value.ignore.length) {
+                let parentNode = $('#' + binding.value.ignore[i])[0];
+                ignore = $.contains(parentNode, event.target);
+                i++;
+            }
+            if (!el.contains(event.target) && !ignore) {
+                vnode.context[binding.value.switch] = false;
+            }
+        };
+        document.addEventListener('click', outsideClick);
+        document.addEventListener('touchStart', outsideClick);
+    },
+
+    unbind() {
+        document.removeEventListener('click', outsideClick);
+        document.removeEventListener('touchstart', outsideClick);
+    }
+});
+
 new Vue({
-            el:     '#searchApp',
+            el:      '#searchApp',
             store,
-            created: function() {
+            created: function () {
                 this.$store.dispatch('resolveSlug', $(this.$options.el).data('slug'));
             },
-            render: createElement => createElement(App),
+            render:  createElement => createElement(App),
         });
