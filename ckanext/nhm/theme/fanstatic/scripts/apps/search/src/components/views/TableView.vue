1976e1305d9228c0f3218deba00f66f99ef6bcc9
Alice Butcher <alycejenni@gmail.com>
2019-12-04 15:38:11 +0000

    Add horizontal scrolling arrows to table view

--- ckanext/nhm/theme/fanstatic/scripts/apps/search/src/components/views/TableView.vue
+++ ckanext/nhm/theme/fanstatic/scripts/apps/search/src/components/views/TableView.vue
@@ -1,44 +1,64 @@
 <template>
-    <div class="table-grid"
-        :style="{gridTemplateColumns: 'repeat(4, max-content)' + (headers.length > 0 ? `repeat(${headers.length}, auto)` : '')}">
-        <div class="th small-column"></div>
-        <div class="th small-column">Dataset</div>
-        <div class="th small-column">Resource</div>
-        <div class="th small-column last-small-column">Record</div>
-        <div class="th data-header" v-for="(headerGroup, index) in headers" :key="headerGroup.id">
-            <div>
-                <span v-for="header in headerGroup" :key="header.id" class="term-group">
-                    {{ header }}
-                </span>
+    <div style="position: relative;">
+        <div class="scrolling-arrows">
+            <div class="scroll-left"
+                 @click="tableScroll(-100)"
+                 :class="{'disabled': !scrollableLeft}">
+                <i class="fas fa-angle-double-left"></i>
             </div>
-            <div class="flex-container flex-nowrap flex-equal">
-                <i class="delete-field fas fa-times-circle fa-xs"
-                    @click="removeHeader(index)"></i>
-                <i class="move-field fas fa-chevron-circle-left fa-xs"
-                    @click="moveHeader({ix: index, by: -1})" v-if="index > 0"></i>
-                <i class="move-field fas fa-chevron-circle-right fa-xs"
-                    @click="moveHeader({ix: index, by: 1})" v-if="index < (headers.length - 1)"></i>
+            <div class="scroll-right"
+                 @click="tableScroll(100)"
+                 :class="{'disabled': !scrollableRight}">
+                <i class="fas fa-angle-double-right"></i>
             </div>
-        </div>
 
-        <template v-for="(item, ix) in records">
-            <div class="td small-column">{{ (page * 100) + ix + 1 }}</div>
-            <div class="td small-column"><a :href="getDetails(item.resource).packageUrl">
-                {{ resourceDetails[item.resource].package_name }}
-            </a></div>
-            <div class="td small-column"><a :href="getDetails(item.resource).resourceUrl">
-                {{ resourceDetails[item.resource].name }}
-            </a></div>
-            <div class="td small-column"><a
-                :href="`${getDetails(item.resource).resourceUrl}/record/${item.data._id}`">
-                {{ item.data._id }}
-            </a></div>
-            <div class="td" v-for="headerGroup in headers" :key="headerGroup.id">
-                <span v-for="header in headerGroup" :key="header.id" class="term-group">
-                    {{ getValue(item.data, header) || '--' }}
-                </span>
+        </div>
+        <div class="table-grid" @resize="scrollable"
+             ref="tableGrid"
+             :style="{gridTemplateColumns: 'repeat(4, max-content)' + (headers.length > 0 ? `repeat(${headers.length}, auto)` : '')}">
+            <div class="th small-column"></div>
+            <div class="th small-column">Dataset</div>
+            <div class="th small-column">Resource</div>
+            <div class="th small-column last-small-column">Record</div>
+            <div class="th data-header"
+                 v-for="(headerGroup, index) in headers"
+                 :key="headerGroup.id">
+                <div>
+                    <span v-for="header in headerGroup" :key="header.id" class="term-group">
+                        {{ header }}
+                    </span>
+                </div>
+                <div class="flex-container flex-nowrap flex-equal">
+                    <i class="delete-field fas fa-times-circle fa-xs"
+                       @click="removeHeader(index)"></i>
+                    <i class="move-field fas fa-chevron-circle-left fa-xs"
+                       @click="moveHeader({ix: index, by: -1})"
+                       v-if="index > 0"></i> <i class="move-field fas fa-chevron-circle-right fa-xs"
+                                                @click="moveHeader({ix: index, by: 1})"
+                                                v-if="index < (headers.length - 1)"></i>
+                </div>
             </div>
-        </template>
+
+            <template v-for="(item, ix) in records">
+                <div class="td small-column">{{ (page * 100) + ix + 1 }}</div>
+                <div class="td small-column"><a :href="getDetails(item.resource).packageUrl"> {{
+                                                                                              resourceDetails[item.resource].package_name
+                                                                                              }} </a>
+                </div>
+                <div class="td small-column"><a :href="getDetails(item.resource).resourceUrl"> {{
+                                                                                               resourceDetails[item.resource].name
+                                                                                               }} </a>
+                </div>
+                <div class="td small-column">
+                    <a :href="`${getDetails(item.resource).resourceUrl}/record/${item.data._id}`">
+                        {{ item.data._id }} </a></div>
+                <div class="td" v-for="headerGroup in headers" :key="headerGroup.id">
+                    <span v-for="header in headerGroup" :key="header.id" class="term-group">
+                        {{ getValue(item.data, header) || '--' }}
+                    </span>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
@@ -46,7 +66,35 @@
     import BaseView from './BaseView.vue';
 
     export default {
-        extends:    BaseView,
-        name:       'TableView'
+        extends:  BaseView,
+        name:     'TableView',
+        data:     function () {
+            return {
+                scrollableLeft:  false,
+                scrollableRight: false
+            }
+        },
+        computed: {
+            tableGrid() {
+                return $(this.$refs.tableGrid);
+            }
+        },
+        methods:  {
+            tableScroll(amount) {
+                this.tableGrid.scrollLeft(this.tableGrid.scrollLeft() + amount);
+                this.scrollable();
+            },
+            scrollable() {
+                this.scrollableLeft  = this.tableGrid.scrollLeft() > 0;
+                let fullWidth        = this.$refs.tableGrid.scrollWidth;
+                let viewWidth        = this.tableGrid.width();
+                this.scrollableRight = fullWidth > viewWidth && this.tableGrid.scrollLeft() < (fullWidth - viewWidth);
+            }
+        },
+        mounted() {
+            // give the table a second to load properly...
+            setTimeout(this.scrollable, 1000);
+            $(window).resize(this.scrollable);
+        }
     }
 </script>
