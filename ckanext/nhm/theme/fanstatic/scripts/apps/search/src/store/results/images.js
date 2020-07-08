cc2f65c03a2a7572dbf827bc7ded43cdf24a58fc
Ginger Butcher <alycejenni@gmail.com>
2020-07-08 17:28:39 +0100

    Remove the checks for broken images
    It didn't work and was probably slow

--- ckanext/nhm/theme/fanstatic/scripts/apps/search/src/store/results/images.js
+++ ckanext/nhm/theme/fanstatic/scripts/apps/search/src/store/results/images.js
@@ -6,8 +6,6 @@ let images = {
     getters:    {
         getItemImages: (state, getters, rootState, rootGetters) => (item, first, recordIndex) => {
             let images;
-            const noImageSize = 1164;
-            const noImageHash = 'b67da4c749bc536ddf27339cb0ec4e63';
 
             let resourceDetails = rootGetters['results/query/resources/resourceDetails'][item.resource];
             let recordUrl       = `${resourceDetails.resourceUrl}/record/${item.data._id}`;
@@ -21,21 +19,6 @@ let images = {
                             {title: img.license, url: img.license};
                         let imgThumb   = img.identifier.replace('preview', 'thumbnail');
                         let isBroken   = false;
-                        axios.get(imgThumb.replace('media-store', 'media-store1'), {responseType: 'blob'})
-                             .then(d => {
-                                 if (d.data.size === noImageSize) {
-                                     console.log(imgThumb)
-                                     let fileReader       = new FileReader();
-                                     fileReader.onloadend = function () {
-                                         let imageHash = SparkMD5.ArrayBuffer.hash(fileReader.result);
-                                         isBroken      = imageHash === noImageHash;
-
-                                     }
-                                     fileReader.readAsArrayBuffer(d.data);
-                                 }
-                             }).catch(e => {
-                            console.log(e)
-                        })
                         return {
                             preview:  img.identifier,
                             thumb:    imgThumb,
