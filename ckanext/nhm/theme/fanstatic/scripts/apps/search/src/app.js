33da537a200551fc7f6080a508a4ae53185d33f7
Josh Humphries <j.humphries@nhm.ac.uk>
2020-09-04 13:17:08 +0100

    Put dismiss directive back in, woops

--- ckanext/nhm/theme/fanstatic/scripts/apps/search/src/app.js
+++ ckanext/nhm/theme/fanstatic/scripts/apps/search/src/app.js
@@ -21,35 +21,35 @@ Vue.use(VueClipboard);
 Vue.use(VueRouter);
 
 // directives
-// let outsideClick = {};
-// Vue.directive('dismiss', {
-//     bind(el, binding, vnode) {
-//         outsideClick[vnode.context._uid] = (event) => {
-//             event.stopPropagation();
-//             if (!$.contains(el, event.target)) {
-//                 let ignore = binding.value.ignore.some(selector => event.target.matches(selector));
-//                 let i      = 0;
-//                 while (!ignore && i < binding.value.ignore.length) {
-//                     let parentNode = $(binding.value.ignore[i])[0];
-//                     if (parentNode !== undefined) {
-//                         ignore = $.contains(parentNode, event.target);
-//                     }
-//                     i++;
-//                 }
-//                 if (!ignore) {
-//                     vnode.context[binding.value.switch] = false;
-//                 }
-//             }
-//         };
-//         document.addEventListener('click', outsideClick[vnode.context._uid]);
-//         document.addEventListener('touchStart', outsideClick[vnode.context._uid]);
-//     },
-//
-//     unbind(el, binding, vnode) {
-//         document.removeEventListener('click', outsideClick[vnode.context._uid]);
-//         document.removeEventListener('touchstart', outsideClick[vnode.context._uid]);
-//     }
-// });
+let outsideClick = {};
+Vue.directive('dismiss', {
+    bind(el, binding, vnode) {
+        outsideClick[vnode.context._uid] = (event) => {
+            event.stopPropagation();
+            if (!$.contains(el, event.target)) {
+                let ignore = binding.value.ignore.some(selector => event.target.matches(selector));
+                let i      = 0;
+                while (!ignore && i < binding.value.ignore.length) {
+                    let parentNode = $(binding.value.ignore[i])[0];
+                    if (parentNode !== undefined) {
+                        ignore = $.contains(parentNode, event.target);
+                    }
+                    i++;
+                }
+                if (!ignore) {
+                    vnode.context[binding.value.switch] = false;
+                }
+            }
+        };
+        document.addEventListener('click', outsideClick[vnode.context._uid]);
+        document.addEventListener('touchStart', outsideClick[vnode.context._uid]);
+    },
+
+    unbind(el, binding, vnode) {
+        document.removeEventListener('click', outsideClick[vnode.context._uid]);
+        document.removeEventListener('touchstart', outsideClick[vnode.context._uid]);
+    }
+});
 
 // routes
 const RouterWrapper = { template: '<router-view></router-view>'}
