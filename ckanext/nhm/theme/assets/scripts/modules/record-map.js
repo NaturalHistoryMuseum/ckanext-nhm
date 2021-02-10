3e5c45d3da0a01ad5d492af6a6115ff625ae4eb7
Josh Humphries <j.humphries@nhm.ac.uk>
2021-02-10 10:50:05 +0000

    Replace the bits of functionality from ckanext-spatial we must have
    ckanext-spatial hasn't been updated to work with python3 yet which
    means until that happens we can't use it. This is both annoying and
    also not the end of the world as we actually use very little of the
    functionality it provides.
    
    The bits of functionality we use (afaik) are:
    
        - datasets can have an extent specified in geojson
        - dataset extents are shown on the package's page (in the sidebar)
        - specimen records with a lat and lon have a map shown on their
          record pages
        - you can perform geo searches using the API to find datasets in
          the provided area
    
    Of these features, only the record page maps are critical. This is
    because, currently, we've locked down the ability to add an extent
    to a dataset to only admin users and therefore the only dataset with
    an extent is the specimen collection. This makes both the ability to
    add an extent to a dataset and the ability to query datasets based on
    their extent pretty useless currently. Hence, the record page map is
    the only useful feature we use right now.
    
    Therefore, this commit implements a record page map in much the same
    way as we did before using ckanext-spatial but with some of the fat
    trimmed out (e.g. there aren't any config settings available because
    only we're going to use this extension so it might as well be hard
    coded in).
    
    When, in the future, ckanext-spatial is made python3 compatible we
    can evaluate whether we want to bring it back into the fold, however,
    it is worth noting that not having this extension does reduce the
    complexity of our postgresql install as we don't need the postgis
    extension* anymore.
    
    *not having to include the postgis extension is very handy for running
    tests as it solves this issue: https://github.com/ckan/ckan/issues/4764
    where cleaning the database automatically between tests using the
    "clean_db" fixture fails because it tried to delete the spatial_ref_sys
    table and can't.

---
+++ ckanext/nhm/theme/assets/scripts/modules/record-map.js
@@ -0,0 +1,45 @@
+/**
+ * Record map module.
+ * This is essentially a copy of the ckanext-spatial's dataset-map module. This has been copied into
+ * here because ckanext-spatial hasn't been upgraded to Python3 support yet and therefore we can't
+ * use it yet. We've tidied the module up a bit and streamlined it to our specific needs given that
+ * it's now in our ckanext-nhm repo and no one else is going to be using it, this is most noticeable
+ * in the fact we've removed the options that come from the ckan config and just hard coded the
+ * settings we want to use.
+ */
+this.ckan.module('record-map', function () {
+
+    return {
+        initialize: function () {
+            const extent = this.el.data('extent');
+
+            if (!!extent) {
+                const map = new L.Map('record-map-container');
+
+                // create the base tile layer
+                const baseLayerUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+                const baseLayer = new L.TileLayer(baseLayerUrl, {maxZoom: 18, subdomains: 'abc'});
+                map.addLayer(baseLayer);
+
+                // setup the icon point we want to use to show the location of the specimen
+                const pointOptions = {
+                    iconUrl: '/images/leaflet/marker-icon-2x.png',
+                    iconSize: [28, 50],
+                    iconAnchor: [14, 50]
+                };
+                const ckanIcon = L.Icon.extend({options: pointOptions});
+
+                // create the layer that shows the point and add it to the map
+                const extentLayer = L.geoJson(extent, {
+                    pointToLayer: function (feature, latLng) {
+                        return new L.Marker(latLng, {icon: new ckanIcon});
+                    }
+                });
+                extentLayer.addTo(map);
+
+                // ensure we get a nice view of the marker
+                map.setView(L.latLng(extent.coordinates[1], extent.coordinates[0]), 9);
+            }
+        }
+    }
+});
