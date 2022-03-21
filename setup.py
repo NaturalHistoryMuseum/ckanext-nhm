2068fbc68929bf5f03a3988626136bb69197cc64
Ginger Butcher <alycejenni@gmail.com>
2022-03-21 10:09:39 +0000

    Update dependencies and version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from collections import OrderedDict
 from setuptools import find_packages, setup
 
-__version__ = '3.0.25'
+__version__ = '3.0.26'
 
 with open('README.md', 'r') as f:
     __long_description__ = f.read()
@@ -25,10 +25,10 @@ ckan_extensions = OrderedDict([
     github('ckanext-gallery', 'v2.0.1'),
     github('ckanext-gbif', 'v2.0.0'),
     github('ckanext-graph', 'v2.0.0'),
-    github('ckanext-ldap', 'v3.0.1'),
-    github('ckanext-query-dois', 'v2.0.4'),
-    github('ckanext-statistics', 'v2.0.3'),
-    github('ckanext-versioned-datastore', 'v3.1.3'),
+    github('ckanext-ldap', 'v3.1.0'),
+    github('ckanext-query-dois', 'v2.1.0'),
+    github('ckanext-statistics', 'v2.0.4'),
+    github('ckanext-versioned-datastore', 'v3.2.0'),
 ])
 
 setup(
