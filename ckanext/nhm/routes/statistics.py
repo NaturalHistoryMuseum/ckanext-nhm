954eb813dc245275c09a6014989235a6d1c79250
Ginger Butcher <alycejenni@gmail.com>
2022-04-19 13:04:16 +0100

    Improve graphs
    - replace records graph
    - compress json data for templates
    - add curve to line
    - group by month if  > 100 points

--- ckanext/nhm/routes/statistics.py
+++ ckanext/nhm/routes/statistics.py
@@ -3,6 +3,9 @@
 #
 # This file is part of ckanext-nhm
 # Created by the Natural History Museum in London, UK
+import json
+import zlib
+import base64
 from collections import OrderedDict
 from datetime import datetime
 
@@ -38,52 +41,25 @@ def before_request():
 def resources():
     '''Render the resources statistics page.'''
 
-    # Get the oldest tracking date
-    oldest_created_date = model.Session.query(model.Resource.created, ).order_by(
-        model.Resource.created).limit(1).scalar()
-
-    # If oldest date is none (no stats yet) we don't want to continue
-    if oldest_created_date:
-        # Calc difference between dates
-
-        delta = datetime.now() - oldest_created_date
-
-    # If we have data for more than 31 days, we'll show by month;
-    # otherwise segment by day
+    # segment by day (further grouping can be done by d3)
     toolkit.c.date_interval = 'day'
 
     date_func = func.date_trunc(toolkit.c.date_interval, model.Resource.created)
 
     q = model.Session.query(date_func.label('date'), func.count().label('count'))
 
-    q = q.order_by(date_func)
-    q = q.group_by(date_func)
+    q = q.order_by(date_func).group_by(date_func)
 
-    toolkit.c.graph_options = {
-        'series': {
-            'lines': {
-                'show': True
-            },
-            'points': {
-                'show': True
-            }
-        },
-        'xaxis': {
-            'mode': 'time',
-            'ticks': []
-        },
-        'yaxis': {
-            'tickDecimals': 0
-        }
-    }
-
-    toolkit.c.graph_data = []
+    graph_data = []
     total = 0
 
     for stat in q.all():
         total += stat.count
         formatted_date = stat.date.strftime('%Y-%m-%d')
-        toolkit.c.graph_data.append([formatted_date, total])
+        graph_data.append([formatted_date, total])
+
+    # compress the data
+    toolkit.c.graph_data = base64.b64encode(zlib.compress(json.dumps(graph_data).encode(), level=9))
 
     return toolkit.render('stats/resources.html', {
         'title': 'Resource statistics'
@@ -96,7 +72,11 @@ def records():
 
     toolkit.c.datastore_stats = toolkit.get_action('dataset_statistics')(_context(), {})
 
-    toolkit.c.num_records = get_record_stats()
+    record_stats = get_record_stats()
+    graph_data = [[x['date'].strftime('%Y-%m-%d'), x['count']] for x in record_stats]
+
+    # compress the data
+    toolkit.c.graph_data = base64.b64encode(zlib.compress(json.dumps(graph_data).encode(), level=9))
 
     return toolkit.render('stats/records.html', {
         'title': 'Record statistics'
@@ -108,25 +88,8 @@ def contributors():
     '''
     Render the contributors statistics page.
     '''
-    # default the graph options
-    toolkit.c.graph_options = {
-        'series': {
-            'lines': {
-                'show': True
-            },
-            'points': {
-                'show': True
-            }
-        },
-        'xaxis': {
-            'mode': 'time',
-            'ticks': []
-        },
-        'yaxis': {
-            'tickDecimals': 0
-        }
-    }
-    toolkit.c.graph_data = []
+
+    graph_data = []
 
     # we use solr to get the number of authors for the front page statistics so we'll use it again
     # here to get a per-package authors count. We have to use solr directly to do this because the
@@ -162,10 +125,8 @@ def contributors():
 
     # only do stuff if we have some packages
     if order:
-        # get the earliest package creation date
-        delta = datetime.now() - order[0][1]
-        # if we have data for more than 10 days, we'll show by month; otherwise segment by day
-        extraction_format = '%d/%m/%y'
+        # always segment by day (further grouping can be done by d3)
+        extraction_format = '%Y-%m-%d'
 
         # sum the counts by package creation time based on the extraction format we chose
         grouped_ordered_data = OrderedDict()
@@ -182,7 +143,10 @@ def contributors():
         # run through the data, adding up a total as we go and adding the data to the graph
         for i, (formatted_date, count) in enumerate(grouped_ordered_data.items()):
             total += count
-            toolkit.c.graph_data.append([formatted_date, total])
+            graph_data.append([formatted_date, total])
+
+    # compress the data
+    toolkit.c.graph_data = base64.b64encode(zlib.compress(json.dumps(graph_data).encode(), level=9))
 
     return toolkit.render('stats/contributors.html', {
         'title': 'Contributor statistics'
--- ckanext/nhm/theme/assets/scripts/modules/graphs.js
+++ ckanext/nhm/theme/assets/scripts/modules/graphs.js
@@ -2,7 +2,20 @@ ckan.module('stats_graphs', function ($) {
     return {
         initialize: function () {
             const graphBox = $('#graph-box');
-            const data = this.options.data;
+            const encodedData = this.options.data;
+            const byteChars = atob(encodedData);
+            let byteNums = new Array(byteChars.length);
+            for (let i = 0; i < byteChars.length; i++) {
+                byteNums[i] = byteChars.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNums);
+            const inflatedData = pako.inflate(byteArray, { to: 'string' });
+            let data = JSON.parse(inflatedData).map(d => [new Date(d[0]), d[1]]);
+            let dateGrouping = 'day';
+            if (data.length > 100) {
+                data = d3.rollups(data, v => v.reduce((a, b) => Math.max(a, b[1]), 0), d => d3.timeMonth.round(d[0]));
+                dateGrouping = 'month';
+            }
 
             $(window).on('resize', drawGraph);
             drawGraph();
@@ -17,15 +30,15 @@ ckan.module('stats_graphs', function ($) {
                 let svg = d3.select('#graph-box').append('svg').attr('width', width)
                             .attr('height', height)
                             .attr('viewBox', [0, 0, width + 50, height + 50]);
-                let x = d3.map(data, (d) => Date.parse(d[0]));
+                let x = d3.map(data, (d) => d[0]);
                 let y = d3.map(data, (d) => d[1]);
                 let xScale = d3.scaleUtc(d3.extent(x), [marginH, width - marginH]);
                 let yScale = d3.scaleLinear([0, d3.max(y)], [height - marginV, marginV]);
-                let xAxis = d3.axisBottom(xScale).ticks(width / 50).tickSizeOuter(0);
+                let xAxis = d3.axisBottom(xScale).ticks(d3.timeYear.every(1)).tickSizeOuter(0);
                 let yAxis = d3.axisLeft(yScale).ticks(10);
 
                 let line = d3.line()
-                             .curve(d3.curveLinear)
+                             .curve(d3.curveCatmullRom)
                              .x(i => xScale(x[i]))
                              .y(i => yScale(y[i]));
 
@@ -44,7 +57,8 @@ ckan.module('stats_graphs', function ($) {
                 svg.append('path')
                    .attr('fill', 'none')
                    .attr('stroke', '#188100')
-                   .attr('stroke-width', 2)
+                   .attr('stroke-opacity', 0.5)
+                   .attr('stroke-width', 4)
                    .attr('d', line(d3.range(x.length)));
 
                 svg.append('g')
@@ -67,7 +81,7 @@ ckan.module('stats_graphs', function ($) {
                 let xText = tooltip.append('text');
                 let yText = tooltip.append('text');
 
-                svg.on('pointerenter pointermove', e => {
+                svg.on('pointerenter pointermove', event => {
                     let pointerXY = d3.pointer(event);
                     const pointIx = d3.bisectCenter(x, xScale.invert(pointerXY[0]));
                     const pointX = x[pointIx];
@@ -83,9 +97,12 @@ ckan.module('stats_graphs', function ($) {
 
                     xText.attr('y', '-1em')
                          .attr('x', 5)
-                         .text(d => {
-                             let xDate = new Date(pointX);
-                             return xDate.toLocaleDateString();
+                         .text(() => {
+                             let day = (pointX.getDate()).toString().padStart(2, '0');
+                             let month = (pointX.getMonth() + 1).toString().padStart(2, '0');
+                             let year = pointX.getFullYear();
+                             let dateString = `${ year }-${ month }`;
+                             return dateGrouping === 'day' ? `${ dateString }-${ day }` : dateString;
                          });
 
                     yText.attr('x', 5)
@@ -99,7 +116,7 @@ ckan.module('stats_graphs', function ($) {
                        .attr('y', boxY - 5);
                 });
 
-                svg.on('pointerleave', (e) => {
+                svg.on('pointerleave', () => {
                     tooltip.style('display', 'none');
                     vLine.style('display', 'none');
                 });
