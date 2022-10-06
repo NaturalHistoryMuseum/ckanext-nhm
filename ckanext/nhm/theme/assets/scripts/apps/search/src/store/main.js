debdc79633d6ae7ed294a4bcb945a5fcfe0dfbbb
Ginger Butcher <alycejenni@gmail.com>
2022-10-06 09:52:51 +0100

    Remove vuex-persist
    State seems to persist with back button anyway, which is what we wanted it for. For everything else it's just breaking things.

--- ckanext/nhm/theme/assets/scripts/apps/search/src/store/main.js
+++ ckanext/nhm/theme/assets/scripts/apps/search/src/store/main.js
@@ -1,77 +1,67 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexPersist from 'vuex-persist';
 import results from './results/main';
 import appState from './appState';
 import $RefParser from 'json-schema-ref-parser';
 import * as d3 from 'd3-collection';
-import {post, get} from './utils';
+import { get, post } from './utils';
 import pako from 'pako';
 import router from '../router';
 
 Vue.use(Vuex);
 
-const stateStorage = new VuexPersist({
-                                         key:             'nhm-data-portal',
-                                         storage:         window.sessionStorage,
-                                         reducer:         state => ({
-                                             results: state.results
-                                         }),
-                                         supportCircular: true
-                                     })
 
 const store = new Vuex.Store(
     {
-        modules:   {
+        modules: {
             results,
             appState
         },
-        plugins:   [stateStorage.plugin],
-        state:     {
+        state: {
             schema: {
                 groups: [],
-                terms:  {},
-                raw:    {}
+                terms: {},
+                raw: {}
             }
         },
-        getters:   {
+        getters: {
             getGroup: state => groupId => {
                 return {
                     'and': 'ALL OF',
-                    'or':  'ANY OF',
+                    'or': 'ANY OF',
                     'not': 'NONE OF'
                 }[groupId];
             },
         },
         mutations: {},
-        actions:   {
+        actions: {
             getSchema(context) {
                 context.state.appState.app.loading = true;
-                context.state.appState.app.error   = false;
-                let schemaPath                     = '/querySchemas/v1.0.0/v1.0.0.json';
-                return $RefParser.dereference(schemaPath, {resolve: {http: {timeout: 2000}}})
-                          .then(data => {
-                              let groups = d3.keys(data.definitions.group.properties);
-                              let terms  = d3.nest()
-                                             .key(t => t.key.includes('_') ? t.key.split('_')[0] : 'other')
-                                             .rollup(leaves => leaves.map((l) => l.key.slice(l.key.indexOf('_') + 1) || ''))
-                                             .object(d3.entries(data.definitions.term.properties));
-                              return {
-                                  groups: groups,
-                                  terms:  terms,
-                                  raw:    data
-                              };
-                          })
-                          .then(schema => {
-                              context.state.schema               = schema;
-                              context.state.appState.app.loading = false;
-                          }, error => {
-                              context.state.appState.app.loading = false;
-                              context.state.appState.app.error   = true;
-                          });
+                context.state.appState.app.error = false;
+                let schemaPath = '/querySchemas/v1.0.0/v1.0.0.json';
+                return $RefParser.dereference(schemaPath, { resolve: { http: { timeout: 2000 } } })
+                                 .then(data => {
+                                     let groups = d3.keys(data.definitions.group.properties);
+                                     let terms = d3.nest()
+                                                   .key(t => t.key.includes('_') ? t.key.split('_')[0] : 'other')
+                                                   .rollup(leaves => leaves.map((l) => l.key.slice(l.key.indexOf('_') + 1) || ''))
+                                                   .object(d3.entries(data.definitions.term.properties));
+                                     return {
+                                         groups: groups,
+                                         terms: terms,
+                                         raw: data
+                                     };
+                                 })
+                                 .then(schema => {
+                                     context.state.schema = schema;
+                                     context.state.appState.app.loading = false;
+                                 }, error => {
+                                     context.state.appState.app.loading = false;
+                                     context.state.appState.app.error = true;
+                                 });
             },
             resolveUrl(context, route) {
-                let slug      = route.params.slug;
+                let slug = route.params.slug;
                 let pageParam = route.query.page;
                 let viewParam = route.query.view;
                 if (slug === undefined || slug === '') {
@@ -88,11 +78,11 @@ const store = new Vuex.Store(
                         if (pageParam !== undefined) {
                             try {
                                 let compressedString = Buffer.from(pageParam, 'base64');
-                                let afterList        = JSON.parse(pako.inflate(compressedString, {to: 'string'}));
+                                let afterList = JSON.parse(pako.inflate(compressedString, { to: 'string' }));
                                 if (afterList.length > 1) {
                                     afterList.forEach(a => {
-                                        context.commit('results/addPage', a)
-                                    })
+                                        context.commit('results/addPage', a);
+                                    });
                                     page = afterList.length - 1;
                                 }
                             } catch (error) {
@@ -103,14 +93,13 @@ const store = new Vuex.Store(
                             context.commit('results/display/setView', viewParam);
                         }
                         context.dispatch('results/runSearch', page);
-                    }
-                    else {
+                    } else {
                         throw Error;
                     }
                 }).catch(error => {
                     context.commit('results/query/filters/resetFilters');
                 }).finally(() => {
-                    router.replace({query: {}, params: {}, path: '/search'})
+                    router.replace({ query: {}, params: {}, path: '/search' });
                 });
             },
             getUser(context) {
@@ -118,7 +107,7 @@ const store = new Vuex.Store(
                     return {
                         loggedIn: true,
                         sysAdmin: d.result.sysadmin
-                    }
+                    };
                 }).catch(e => {
                     return {
                         loggedIn: false,
