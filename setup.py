36262007ef2e0c8b68ed9137ea2e44a257b5f992
ginger <alycejenni@gmail.com>
2022-10-24 10:34:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from collections import OrderedDict
 from setuptools import find_packages, setup
 
-__version__ = '3.7.0'
+__version__ = '3.7.1'
 
 with open('README.md', 'r') as f:
     __long_description__ = f.read()
