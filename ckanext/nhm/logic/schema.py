f00c95136670fba2a00955e65b06b05ee1065522
Josh Humphries <j.humphries@nhm.ac.uk>
2023-09-12 16:47:40 +0100

    fix: use CKAN validators instead of custom functions + make use of validator_args decorator
    We can't use str and int as schema validator components any more as they've tightened up the rules. Technically, we never should have been using these but in CKAN 2.10 this is now enforced.

--- ckanext/nhm/logic/schema.py
+++ ckanext/nhm/logic/schema.py
@@ -8,17 +8,9 @@ from ckan.logic.schema import (
     default_show_package_schema,
     default_update_package_schema,
 )
+from ckan.logic.schema import validator_args
 from ckan.plugins import toolkit
-
-from ckanext.nhm.logic.validators import string_max_length, uuid_validator
-
-not_empty = toolkit.get_validator('not_empty')
-ignore_missing = toolkit.get_validator('ignore_missing')
-not_missing = toolkit.get_validator('not_missing')
-resource_id_exists = toolkit.get_validator('resource_id_exists')
-int_validator = toolkit.get_validator('int_validator')
-boolean_validator = toolkit.get_validator('boolean_validator')
-one_of = toolkit.get_validator('one_of')
+from ckan.types import Validator
 
 DATASET_TYPE_VOCABULARY = 'dataset_category'
 
@@ -34,20 +26,33 @@ UPDATE_FREQUENCIES = [
 ]
 
 
-def record_show_schema():
+@validator_args
+def record_show_schema(
+    not_missing: Validator,
+    unicode_safe: Validator,
+    resource_id_validator: Validator,
+    int_validator: Validator,
+    ignore_missing: Validator,
+):
     schema = {
-        'resource_id': [not_missing, str, resource_id_exists],
-        'record_id': [not_missing, int_validator],
-        'version': [ignore_missing, int_validator],
+        "resource_id": [not_missing, unicode_safe, resource_id_validator],
+        "record_id": [not_missing, int_validator],
+        "version": [ignore_missing, int_validator],
     }
     return schema
 
 
-def object_rdf_schema():
+@validator_args
+def object_rdf_schema(
+    not_missing: Validator,
+    unicode_safe: Validator,
+    int_validator: Validator,
+    ignore_missing: Validator,
+):
     schema = {
-        'uuid': [not_missing, str],
-        'format': [not_missing],
-        'version': [ignore_missing, int_validator],
+        "uuid": [not_missing, unicode_safe],
+        "format": [not_missing],
+        "version": [ignore_missing, int_validator],
     }
     return schema
 
@@ -65,50 +70,65 @@ def update_package_schema():
 
 
 def _modify_schema(schema):
-    convert_from_tags = toolkit.get_converter('convert_to_tags')
-    convert_to_extras = toolkit.get_converter('convert_to_extras')
+    convert_from_tags = toolkit.get_converter("convert_to_tags")
+    convert_to_extras = toolkit.get_converter("convert_to_extras")
+    unicode_safe = toolkit.get_validator("unicode_safe")
+    not_empty = toolkit.get_validator("not_empty")
+    ignore_missing = toolkit.get_validator("ignore_missing")
+    one_of = toolkit.get_validator("one_of")
+    boolean_validator = toolkit.get_validator("boolean_validator")
+
     # Required fields
-    schema['title'] = [not_empty, string_max_length(255), str]
-    schema['notes'] = [not_empty, string_max_length(4000), str]
-    schema['author'] = [not_empty, str]
-    schema['resources']['name'] = [not_empty, string_max_length(255), str]
+    schema["title"] = [not_empty, unicode_safe]
+    schema["notes"] = [not_empty, unicode_safe]
+    schema["author"] = [not_empty, unicode_safe]
+    schema["resources"]["name"] = [not_empty, unicode_safe]
+
     # Add new fields
     schema[DATASET_TYPE_VOCABULARY] = [
         not_empty,
         convert_from_tags(DATASET_TYPE_VOCABULARY),
     ]
-    schema['temporal_extent'] = [ignore_missing, str, convert_to_extras]
-    schema['affiliation'] = [ignore_missing, str, convert_to_extras]
-    schema['contributors'] = [ignore_missing, str, convert_to_extras]
-    schema['update_frequency'] = [
+    schema["temporal_extent"] = [ignore_missing, unicode_safe, convert_to_extras]
+    schema["affiliation"] = [ignore_missing, unicode_safe, convert_to_extras]
+    schema["contributors"] = [ignore_missing, unicode_safe, convert_to_extras]
+    schema["update_frequency"] = [
         ignore_missing,
         one_of([v[0] for v in UPDATE_FREQUENCIES]),
         convert_to_extras,
-        str,
+        unicode_safe,
     ]
-    schema['promoted'] = [ignore_missing, convert_to_extras, boolean_validator]
-    schema['spatial'] = [ignore_missing, convert_to_extras]
+    schema["promoted"] = [ignore_missing, convert_to_extras, boolean_validator]
+    schema["spatial"] = [ignore_missing, convert_to_extras]
 
 
-def show_package_schema():
-    convert_from_extras = toolkit.get_converter('convert_from_extras')
-    convert_to_tags = toolkit.get_converter('convert_from_tags')
+@validator_args
+def show_package_schema(
+    ignore_missing: Validator,
+    convert_from_extras: Validator,
+    convert_to_tags: Validator,
+):
     schema = default_show_package_schema()
-    schema['tags']['__extras'].append(toolkit.get_converter('free_tags_only'))
+    schema["tags"]["__extras"].append(toolkit.get_converter("free_tags_only"))
     schema[DATASET_TYPE_VOCABULARY] = [convert_to_tags(DATASET_TYPE_VOCABULARY)]
-    schema['temporal_extent'] = [convert_from_extras, ignore_missing]
-    schema['update_frequency'] = [convert_from_extras, ignore_missing]
-    schema['affiliation'] = [convert_from_extras, ignore_missing]
-    schema['contributors'] = [convert_from_extras, ignore_missing]
-    schema['promoted'] = [convert_from_extras, ignore_missing]
+    schema["temporal_extent"] = [convert_from_extras, ignore_missing]
+    schema["update_frequency"] = [convert_from_extras, ignore_missing]
+    schema["affiliation"] = [convert_from_extras, ignore_missing]
+    schema["contributors"] = [convert_from_extras, ignore_missing]
+    schema["promoted"] = [convert_from_extras, ignore_missing]
     # This is the same as the extras field with key=spatial for ckanext-spatial
-    schema['spatial'] = [convert_from_extras, ignore_missing]
+    schema["spatial"] = [convert_from_extras, ignore_missing]
     return schema
 
 
-def get_permanent_url_schema():
+@validator_args
+def get_permanent_url_schema(
+    not_missing: Validator,
+    ignore_missing: Validator,
+    boolean_validator: Validator,
+):
     return {
-        'field': [not_missing, str],
-        'value': [not_missing, str],
-        'include_version': [ignore_missing, boolean_validator],
+        "field": [not_missing, str],
+        "value": [not_missing, str],
+        "include_version": [ignore_missing, boolean_validator],
     }
