68a936085c8aac8fe413fd577a787a1538adca59
Alice Butcher <alycejenni@gmail.com>
2023-10-12 09:59:52 +0100

    fix: add a button to add currently selected field
    Closes #706

--- ckanext/nhm/theme/assets/scripts/apps/search/src/components/misc/FieldPicker.vue
+++ ckanext/nhm/theme/assets/scripts/apps/search/src/components/misc/FieldPicker.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="space-children-v field-picker" :class="classes">
-    <input
-      type="text"
+    <div class="flex-container flex-nowrap flex-stretch-first">
+      <input
+        type="text"
+        class="full-width"
+        name="fieldSearch"
+        id="fieldSearch"
+        title="Search for a field by prefix"
+        value=""
+        autocomplete="off"
+        placeholder="field name"
+        v-model="fieldSearch"
+      />
+      <button class="btn btn-mini" @click="addSelectedField">
+        <i class="fas fa-arrow-circle-up no-margin"></i>
+      </button>
+    </div>
+    <select
       class="full-width"
-      name="fieldSearch"
-      id="fieldSearch"
-      title="Search for a field by prefix"
-      value=""
-      autocomplete="off"
-      placeholder="field name"
-      v-model="fieldSearch"
-    />
-    <select class="full-width" size="10" title="Select a field">
+      size="10"
+      title="Select a field"
+      v-model="selectedField"
+      @keyup.enter="addSelectedField"
+    >
       <option
         v-for="field in fieldList"
         v-bind:key="field.id"
@@ -33,6 +44,7 @@ export default {
     return {
       fieldSearch: null,
       fieldList: [],
+      selectedField: null,
     };
   },
   props: ['callback', 'resourceIds', 'classes'],
@@ -57,6 +69,9 @@ export default {
         .catch((error) => {
           vue.fieldList = [];
         });
+    },
+    addSelectedField() {
+      this.callback(this.selectedField);
     },
   },
   mounted: function () {
