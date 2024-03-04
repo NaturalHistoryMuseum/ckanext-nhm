d40934e7a885d95c55500b291cb8fe4675e81d05
Alice Butcher <alycejenni@gmail.com>
2024-03-04 13:10:33 +0000

    fix: rate limit api calls from liv
    makes it slightly slower, but also prevents it making a thousand calls a second if the images aren't working

--- ckanext/nhm/theme/assets/scripts/apps/liv/src/utils/api.js
+++ ckanext/nhm/theme/assets/scripts/apps/liv/src/utils/api.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
+import rateLimit from 'axios-rate-limit';
 
-const api = axios.create({
-  baseURL: '/api/3/action/',
-  timeout: 20000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const api = rateLimit(
+  axios.create({
+    baseURL: '/api/3/action/',
+    timeout: 20000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }),
+  { maxRPS: 5 },
+);
 
 export async function post(action, body) {
   return api.post(action, body).then((response) => {
