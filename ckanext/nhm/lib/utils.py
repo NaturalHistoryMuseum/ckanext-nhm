db08f3213a1bf3e8e610517c7627a4d152cb8c08
AndrewRoberts1 <andy.direct101@gmail.com>
2026-02-23 13:47:31 +0000

    fix(status): code clean up and add user agent to gbif request
    Closes: #841

--- ckanext/nhm/lib/utils.py
+++ ckanext/nhm/lib/utils.py
@@ -4,8 +4,8 @@
 # This file is part of ckanext-nhm
 # Created by the Natural History Museum in London, UK
 
-from datetime import datetime, timedelta, timezone
 from datetime import datetime as dt
+from datetime import timedelta, timezone
 
 import requests
 from cachetools import TTLCache, cached
@@ -87,28 +87,36 @@ def get_ingest_status():
 
 @cached(cache=TTLCache(maxsize=10, ttl=7200))
 def get_gbif_status():
-    gbif_url = toolkit.config.get('ckanext.gbif.dataset_url')
+    gbif_dataset_key = toolkit.config.get('ckanext.gbif.dataset_key')
+    gbif_url = (
+        f'https://api.gbif.org/v1/dataset/{gbif_dataset_key}'
+        if gbif_dataset_key
+        else None
+    )
+
+    status_text = 'unknown'
+    status_type = 'bad'
 
     if gbif_url:
         try:
-            r = requests.get(gbif_url, timeout=7)
+            headers = {'User-Agent': 'NHMUK dataset status: data [at] nhm.ac.uk'}
+            r = requests.get(gbif_url, timeout=7, headers=headers)
             if r.ok:
                 response_json = r.json()
                 modified = response_json.get('modified')
+            else:
+                modified = None
         except Exception as e:
             modified = None
 
-    if modified:
-        modified_datetime = datetime.strptime(str(modified), '%Y-%m-%dT%H:%M:%S.%f%z')
-        now = datetime.now(timezone.utc)
-        time_diff = now - modified_datetime
-        # Check if modified date is within last 7 days
-        if time_diff < timedelta(days=7):
-            status_type = 'good'
-        # Display last modified date
-        status_text = modified_datetime.strftime('%Y-%m-%d')
-    else:
-        status_text = 'unknown'
-        status_type = 'bad'
+        if modified:
+            modified_datetime = dt.strptime(str(modified), '%Y-%m-%dT%H:%M:%S.%f%z')
+            now = dt.now(timezone.utc)
+            time_diff = now - modified_datetime
+            # Check if modified date is within last 7 days
+            if time_diff < timedelta(days=7):
+                status_type = 'good'
+            # Display last modified date
+            status_text = modified_datetime.strftime('%Y-%m-%d')
 
     return status_text, status_type
